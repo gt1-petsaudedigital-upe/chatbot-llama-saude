3aa41e1fcc56a7cc3a94866227711bb43bdbf9cb
jorge <carvalhojorge501@gmail.com>
2026-04-08 11:12:05 -0300

    solve test module problem

--- src/groq/groq.service.spec.ts
+++ src/groq/groq.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GroqService } from './groq.service';
+import { ConfigModule } from '@nestjs/config';
+import { beforeEach, describe, it } from 'node:test';
 
 describe('GroqService', () => {
   let service: GroqService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()], // ← estava faltando isso
       providers: [GroqService],
     }).compile();
 
